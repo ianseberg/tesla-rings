cdd07a6d85044c1b0275affa22fca06c34d4763d
ianseberg <daniele.coassin@gmail.com>
2020-12-04 01:15:40 +0100

    v0.0.2

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -3,7 +3,7 @@ var plugins = [{
       options: {"plugins":[]},
     },{
       plugin: require('/Users/flamingo/tesla-rings/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"Eventually","short_name":"Landing","start_url":"/gatsby-starter-eventually/","background_color":"#663399","theme_color":"#663399","display":"standalone","icon":"src/assets/img/website-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"4a8a0b61aaa0c3e59cc0cb66492fa2f2"},
+      options: {"plugins":[],"name":"Tesla rings","short_name":"teslarings","start_url":"/tesla-rings/","background_color":"#663399","theme_color":"#663399","display":"standalone","icon":"src/assets/img/website-icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"4a8a0b61aaa0c3e59cc0cb66492fa2f2"},
     },{
       plugin: require('/Users/flamingo/tesla-rings/node_modules/gatsby-plugin-offline/gatsby-ssr'),
       options: {"plugins":[]},
